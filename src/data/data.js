b8342b335f6af7b8bece0048548b7632a76c72f5
Usman-Kashmiri <usmankashmiri378@gmail.com>
2023-05-13 17:54:07 +0500

    working on payment activity page

--- src/data/data.js
+++ src/data/data.js
@@ -5,3 +5,22 @@ export const sideNav = [
         icon:<i className="fa fa-home"></i>
     }
 ]
+
+export const selectOptions = [
+    {
+        value: "113949522673653",
+        label: "Port Grand (113949522673653)"
+    },
+    {
+        value: "113949522673652",
+        label: "Port Grand (113949522673652)"
+    },
+    {
+        value: "113949522673651",
+        label: "Port Grand (113949522673651)"
+    },
+    {
+        value: "113949522673650",
+        label: "Port Grand (113949522673650)"
+    },
+]
