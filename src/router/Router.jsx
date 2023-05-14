5776240f553e0c534653672990265b8964610fab
Usman-Kashmiri <usmankashmiri378@gmail.com>
2023-05-14 12:39:19 +0500

    Content page 20% completed

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from '../pages/Home'
 import PaymentActivity from '../pages/PaymentActivity'
+import Content from '../pages/Content'
 
 const Router = () => {
   return (
@@ -9,6 +10,7 @@ const Router = () => {
     <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/payment-activity' element={<PaymentActivity />} />
+        <Route path='/content' element={<Content />} />
     </Routes>
    
   )
