38ffddbed7a8cda3046c149952d005b55320213b
Usman-Kashmiri <usmankashmiri378@gmail.com>
2023-05-17 17:09:51 +0500

    Working on transaction detail page

--- src/components/alltoolsdetail.jsx
+++ src/components/alltoolsdetail.jsx
@@ -1,95 +1,139 @@
-
-
-
-
-
-
-import React from 'react'
-import './alltool.css'
-import { Row ,Col } from 'react-bootstrap'
-import content from './images/contenticon.png'
+import React from "react";
+import "./alltool.css";
+import { Row, Col } from "react-bootstrap";
+import content from "./images/contenticon.png";
 // import Sidebar from '../components/sidebar'
-import { engageAudience ,manage } from '../data/data'
+import { engageAudience, manage } from "../data/data";
+import { useNavigate } from "react-router-dom";
 // import Homedetail from '../components/homedetail'
 
-
 const Alltooldetails = () => {
+  const navigate = useNavigate();
+
   return (
-    <div style={{ width:'100%',backgroundColor:'#f5fafe'}}>
-        <div className='searchbox'>
+    <div style={{ width: "100%", backgroundColor: "#f5fafe" }}>
+      <div className="searchbox">
         <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="20"
-      height="20"
-      fill="none"
-      viewBox="0 0 24 24"
-    >
-      <g>
-        <path
-          stroke="#000"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          d="M15 15l6 6m-11-4a7 7 0 110-14 7 7 0 010 14z"
-        ></path>
-      </g>
-    </svg>
-    <input className='searchinput' placeholder='Search all tool for keywords'/>
-        </div>
-     <Row style={{ width:'90%' , marginLeft:'10%'}}>
-        <Col lg={6} >
-        <p className='toolheadingone'>Engaged Audience</p>
-        {
-            engageAudience.map(( value , i) => {
-                return(
-        <div style={{display:'flex' , paddingBottom:'0px'}} key={i}>
-            {value.image}
-            {/* <img  width={25} height={25} src={content}/> */}
-            <div className='toolfirst'>
-                {/* <p> */}
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="20"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <g>
+            <path
+              stroke="#000"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M15 15l6 6m-11-4a7 7 0 110-14 7 7 0 010 14z"
+            ></path>
+          </g>
+        </svg>
+        <input
+          className="searchinput"
+          placeholder="Search all tool for keywords"
+        />
+      </div>
+      <Row style={{ width: "90%", marginLeft: "10%" }}>
+        <Col lg={6}>
+          <p className="toolheadingone">Engaged Audience</p>
+          {engageAudience.map((value, i) => {
+            return (
+              <div style={{ display: "flex", paddingBottom: "0px" }} key={i}>
+                <img
+                  className={
+                    value.heading === "Content" ? "cursor-pointer" : ""
+                  }
+                  width={25}
+                  height={25}
+                  src={value.image}
+                  onClick={
+                    value.heading === "Content"
+                      ? () => navigate("/content")
+                      : null
+                  }
+                />
+                {/* <img  width={25} height={25} src={content}/> */}
+                <div className="toolfirst">
+                  {/* <p> */}
+                  <span
+                    className={
+                      value.heading === "Content" ? "cursor-pointer" : ""
+                    }
+                    onClick={
+                      value.heading === "Content"
+                        ? () => navigate("/content")
+                        : null
+                    }
+                  >
+                    {value.heading}
+                  </span>
+                  {/* </p> */}
 
-                 {value.heading}
-                {/* </p> */}
-                 
-            <p className='tooldetails'>{value.details}</p>
-                 </div>
-            {/* <br /> */}
-            
-        </div>
-                )
-            })
-        }
+                  <p className="tooldetails">{value.details}</p>
+                </div>
+                {/* <br /> */}
+              </div>
+            );
+          })}
 
-        <p className='toolheadingone'>Advertise </p>
+          <p className="toolheadingone">Advertise </p>
         </Col>
         <Col lg={6}>
-        <p className='toolheadingone'>Manage </p>
-        {
-            manage.map(( value , i) => {
-                return(
-        <div style={{display:'flex' , paddingBottom:'0px' , marginBottom:'0px'}} key={i}>
-            {value.image}
-            {/* <img  width={25} height={25} src={content}/> */}
-            <div className='toolfirst'>
-                
-                 {value.heading}
-                 
-            <p className='tooldetails'>{value.details}</p>
-                 </div>
-            {/* <br /> */}
-            
-        </div>
-                )
-            })
-        }
-            {/* <p className='toolfirst'>
+          <p className="toolheadingone">Manage </p>
+          {manage.map((value, i) => {
+            return (
+              <div
+                style={{
+                  display: "flex",
+                  paddingBottom: "0px",
+                  marginBottom: "0px",
+                }}
+                key={i}
+              >
+                <img
+                  className={
+                    value.heading === "Billing" ? "cursor-pointer" : ""
+                  }
+                  width={25}
+                  height={25}
+                  src={value.image}
+                  onClick={
+                    value.heading === "Billing"
+                      ? () => navigate("/payment-activity")
+                      : null
+                  }
+                />
+                {/* <img  width={25} height={25} src={content}/> */}
+                <div className="toolfirst">
+                  <span
+                    className={
+                      value.heading === "Billing" ? "cursor-pointer" : ""
+                    }
+                    onClick={
+                      value.heading === "Billing"
+                        ? () => navigate("/payment-activity")
+                        : null
+                    }
+                  >
+                    <span>{value.heading}</span>
+                    {value?.icon && <img width={25} src={value.icon} />}
+                  </span>
+
+                  <p className="tooldetails">{value.details}</p>
+                </div>
+                {/* <br /> */}
+              </div>
+            );
+          })}
+          {/* <p className='toolfirst'>
                 Setting
                 </p> */}
         </Col>
-     </Row>
-     
+      </Row>
     </div>
-  )
-}
+  );
+};
 
-export default Alltooldetails
+export default Alltooldetails;
--- src/components/layout/DataTable.jsx
+++ src/components/layout/DataTable.jsx
@@ -1,87 +1,111 @@
 import React, { useEffect, useState } from "react";
 import { Table } from "@mantine/core";
 import AuthUser from "../../Config/UserAuth";
-const DataTable = (props) => {
+import { useNavigate } from "react-router-dom";
 
-  let { selectedDate, setSelectedDate, setTotalAmount,totalAmount } = props;
+const DataTable = (props) => {
+  const navigate = useNavigate();
+
+  let { selectedDate, setSelectedDate, setTotalAmount, totalAmount } = props;
   const { http } = AuthUser();
 
   const [data, setData] = useState([]);
   const fetchData = async () => {
     const formdata = new FormData();
-    formdata.append('dateto', selectedDate[0])
-    formdata.append('datefrom', selectedDate[1])
-    http.post('paymentactivity', formdata).then((res) => {
-      setData(res.data.data)
-      setTotalAmount('--')
-    }).catch((e) => {
-
-    })
-  }
+    formdata.append("dateto", selectedDate[0]);
+    formdata.append("datefrom", selectedDate[1]);
+    http
+      .post("paymentactivity", formdata)
+      .then((res) => {
+        setData(res.data.data);
+        setTotalAmount("--");
+      })
+      .catch((e) => {});
+  };
   useEffect(() => {
     fetchData();
   }, [selectedDate]);
   let newtotalAmount = 0;
 
-  const rows = (data.length > 0 ? (data.map((item, i) => {
-    newtotalAmount = (parseFloat(item.amount) + parseFloat(newtotalAmount))
-    console.log(newtotalAmount)
-    setTotalAmount(newtotalAmount);
-    return (
-      <tr key={i}>
-        <td width="15%" className="py-3 px-4 text-info font-poppins">
-          {item.transaction_id}
-        </td>
-        <td className="py-3 px-4 font-poppins">{item.date}</td>
-        <td className="py-3 px-4 font-poppins">Rs {item.amount}</td>
-        <td className="py-3 px-4 font-poppins">
-          <span className="d-flex align-items-center gap-1">
-            <img
-              width={35}
-              src={item.payment_method_icon}
-              alt="mastercard"
-            />
-            <span className="d-flex flex-column">
-              <span>{item.payment_method}</span>
-              <span>{item.payment_token}</span>
-            </span>
-          </span>
-        </td>
-        <td className="py-3 px-4 font-poppins d-flex align-items-center justify-content-center">
-          <span className={item.payment_status + '-status rounded-pill'}>{item.payment_status}</span>
-
-        </td>
-        <td className="py-3 px-4 font-poppins">{item.invoice_id}</td>
-        <td className="py-3 px-4">
-          <button className="py-2 px-3 btn-grey border-0 rounded-3">
-            <i className="fa fa-download"></i>
-          </button>
-        </td>
-      </tr>
-    )
-  })) : ('no data found'));
+  const rows =
+    data.length > 0
+      ? data.map((item, i) => {
+          newtotalAmount = parseFloat(item.amount) + parseFloat(newtotalAmount);
+          console.log(newtotalAmount);
+          setTotalAmount(newtotalAmount);
+          return (
+            <tr key={i}>
+              <td
+                width="15%"
+                className="p-3 text-info font-poppins cursor-pointer"
+                onClick={() =>
+                  navigate("/transaction-details", { state: item })
+                }
+              >
+                {item.transaction_id}
+              </td>
+              <td className="p-3 font-poppins">{item.date}</td>
+              <td className="p-3 font-poppins">Rs {item.amount}</td>
+              <td className="p-3 font-poppins">
+                <span className="d-flex align-items-center gap-1">
+                  <img
+                    width={35}
+                    src={item.payment_method_icon}
+                    alt="payment type"
+                  />
+                  <span className="d-flex flex-column">
+                    <span>
+                      {item.payment_method} · {item?.card_number}
+                    </span>
+                    <span>{item.payment_token}</span>
+                  </span>
+                </span>
+              </td>
+              <td className="p-3 font-poppins d-flex align-items-center justify-content-center">
+                <span className={item.payment_status + "-status rounded-pill"}>
+                  {item.payment_status}
+                </span>
+              </td>
+              <td className="p-3 font-poppins">{item.invoice_id}</td>
+              <td className="p-3">
+                <span className="d-flex align-items-center gap-2">
+                  <button className="py-2 px-3 btn-grey border-0 rounded-3">
+                    <i className="fa fa-download"></i>
+                  </button>
+                  <button className="py-2 px-3 btn-grey border-0 rounded-3">
+                    <i className="fa fa-edit"></i>
+                  </button>
+                  <button className="py-2 px-3 btn-grey border-0 rounded-3">
+                    <i className="fa fa-trash"></i>
+                  </button>
+                </span>
+              </td>
+            </tr>
+          );
+        })
+      : "no data found";
 
   return (
-    <Table>
-      <thead>
-        <tr>
-          <th className="py-3 px-4">Transaction ID</th>
-          <th className="py-3 px-4">
+    <Table className="dataTable">
+      <thead className="position-sticky top-0 bg-white">
+        <tr className="bg-white">
+          <th className="p-3">Transaction ID</th>
+          <th className="p-3">
             Date <i className="text-grey-color fa fa-info-circle"></i>
           </th>
-          <th className="py-3 px-4">
+          <th className="p-3">
             Amount <i className="text-grey-color fa fa-info-circle"></i>
           </th>
-          <th className="py-3 px-4">
+          <th className="p-3">
             Payment method <i className="text-grey-color fa fa-info-circle"></i>
           </th>
-          <th className="py-3 px-4">
+          <th className="p-3">
             Payment status <i className="text-grey-color fa fa-info-circle"></i>
           </th>
-          <th className="py-3 px-4">
+          <th className="p-3">
             VAt invoice ID <i className="text-grey-color fa fa-info-circle"></i>
           </th>
-          <th className="py-3 px-4">Action</th>
+          <th className="p-3">Action</th>
         </tr>
       </thead>
       <tbody>{rows}</tbody>
